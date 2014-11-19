d07f9c8be315606aee136b8b55740cd88a83ee84
jlaverty <jlaverty@myseneca.ca>
2014-11-18 21:02:11 -0500

    Added MapActivity to party_main

--- app/src/main/java/com/example/arnold/partyfi/Parties_Main.java
+++ app/src/main/java/com/example/arnold/partyfi/Parties_Main.java
@@ -1,18 +1,41 @@
 package com.example.arnold.partyfi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Parties_Main extends ActionBarActivity
 {
+    private static String logtag = "Parties_Main";//for use as the tag when logging
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_party_options);
+
+        Button buttonStart = (Button)findViewById(R.id.findParty);
+        buttonStart.setOnClickListener(startListener); // Register the onClick listener with the implementation above
+
     }
+
+    //Create an anonymous implementation of OnClickListener
+    private OnClickListener startListener = new OnClickListener() {
+        public void onClick(View v) {
+            Log.d(logtag,"onClick() called - start button");
+            Toast.makeText(Parties_Main.this, "The Start button was clicked.", Toast.LENGTH_LONG).show();
+            Log.d(logtag,"onClick() ended - start button");
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -31,5 +54,32 @@ public class Parties_Main extends ActionBarActivity
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    protected void onStart() {//activity is started and visible to the user
+        Log.d(logtag,"onStart() called");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {//activity was resumed and is visible again
+        Log.d(logtag,"onResume() called");
+        super.onResume();
+
+    }
+    @Override
+    protected void onPause() { //device goes to sleep or another activity appears
+        Log.d(logtag,"onPause() called");//another activity is currently running (or user has pressed Home)
+        super.onPause();
+
+    }
+    @Override
+    protected void onStop() { //the activity is not visible anymore
+        Log.d(logtag,"onStop() called");
+        super.onStop();
+
+    }
+    @Override
+    protected void onDestroy() {//android has killed this activity
+        Log.d(logtag,"onDestroy() called");
+        super.onDestroy();
     }
 }
